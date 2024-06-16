720ab6a6ce9bc443c65fbf66bfa17d60caf05efb
nallruf <nalzaibaidin@gmail.com>
2024-06-16 15:27:07 +0700

    fix config

--- app.js
+++ app.js
@@ -77,4 +77,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const port = 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 module.exports = app;
--- config/db.js
+++ config/db.js
@@ -3,14 +3,14 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-    const pool = mysql.createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0
-    });
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
-    module.exports = pool;
+module.exports = pool;
